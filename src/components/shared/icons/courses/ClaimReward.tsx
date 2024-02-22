0907b5f8917935f650117200870ccd4ec354aac0
Phong <phongpv@192.168.61.41>
2024-02-22 22:50:05 +0700

    feat: ui courses

---
+++ src/components/shared/icons/courses/ClaimReward.tsx
@@ -0,0 +1,12 @@
+function ClaimReward() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="40" height="41" fill="none" viewBox="0 0 40 41">
+      <rect width="40" height="40" y="0.25" fill="#FCD535" rx="20"></rect>
+      <path
+        fill="#181A20"
+        d="M22 12.755V8.25h-4v4.505l-3.92-3.92-2.827 2.83 3.25 3.252h-5.17v4h9v-4h3.333v4h9v-4h-5.172l3.254-3.254-2.83-2.826L22 12.754v.001zM9.333 22.25v8.667h9V22.25h-9zm12.333 8.667h9V22.25h-9v8.667z"></path>
+    </svg>
+  )
+}
+
+export default ClaimReward
---
+++ src/components/shared/icons/courses/OneStopLearning.tsx
@@ -0,0 +1,93 @@
+function OneStopLearning() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" fill="none" viewBox="0 0 64 64">
+      <path
+        fill="#AEB4BC"
+        d="M49.334 6l2.667 2.667L54.667 6l-2.666-2.667L49.334 6zM3.334 27.333l2 2 2-2-2-2-2 2zM56.666 35.333h2v-2h-2v2z"
+        opacity="0.3"></path>
+      <path
+        fill="url(#paint0_linear_2708_18257)"
+        d="M32 34a2.667 2.667 0 100-5.333A2.667 2.667 0 0032 34z"></path>
+      <path fill="#AEB4BC" d="M24 36l8 8 8-8v20H24V36z" opacity="0.3"></path>
+      <path
+        fill="url(#paint1_linear_2708_18257)"
+        d="M24 36h-5.333C12.776 36 8 40.776 8 46.667V56h16V36z"></path>
+      <path
+        fill="url(#paint2_linear_2708_18257)"
+        d="M40 36v20h16v-9.333C56 40.776 51.224 36 45.333 36H40z"></path>
+      <path
+        fill="url(#paint3_linear_2708_18257)"
+        d="M22 16c0-5.523 4.478-10 10-10 5.523 0 10 4.477 10 10v1.333h2.667V16c0-6.996-5.67-12.667-12.666-12.667S19.334 9.004 19.334 16v1.333h2.667V16z"></path>
+      <path
+        fill="url(#paint4_linear_2708_18257)"
+        d="M47.333 23.905c0 1.893-1.591 3.428-3.555 3.428H42V17h1.778c1.964 0 3.555 1.535 3.555 3.429v3.476z"></path>
+      <path
+        fill="url(#paint5_linear_2708_18257)"
+        d="M16.667 23.905c0 1.893 1.592 3.428 3.555 3.428H22V17h-1.777c-1.964 0-3.556 1.535-3.556 3.429v3.476z"></path>
+      <defs>
+        <linearGradient
+          id="paint0_linear_2708_18257"
+          x1="32.001"
+          x2="32.001"
+          y1="28.667"
+          y2="34"
+          gradientUnits="userSpaceOnUse">
+          <stop stopColor="#F0B90B"></stop>
+          <stop offset="1" stopColor="#F8D33A"></stop>
+        </linearGradient>
+        <linearGradient
+          id="paint1_linear_2708_18257"
+          x1="32"
+          x2="32"
+          y1="56"
+          y2="36"
+          gradientUnits="userSpaceOnUse">
+          <stop stopColor="#76808F"></stop>
+          <stop offset="1" stopColor="#929AA5"></stop>
+        </linearGradient>
+        <linearGradient
+          id="paint2_linear_2708_18257"
+          x1="32"
+          x2="32"
+          y1="56"
+          y2="36"
+          gradientUnits="userSpaceOnUse">
+          <stop stopColor="#76808F"></stop>
+          <stop offset="1" stopColor="#929AA5"></stop>
+        </linearGradient>
+        <linearGradient
+          id="paint3_linear_2708_18257"
+          x1="32.001"
+          x2="32.001"
+          y1="3.333"
+          y2="17.333"
+          gradientUnits="userSpaceOnUse">
+          <stop stopColor="#929AA5"></stop>
+          <stop offset="1" stopColor="#76808F"></stop>
+        </linearGradient>
+        <linearGradient
+          id="paint4_linear_2708_18257"
+          x1="44.667"
+          x2="44.667"
+          y1="27.333"
+          y2="17"
+          gradientUnits="userSpaceOnUse">
+          <stop stopColor="#F0B90B"></stop>
+          <stop offset="1" stopColor="#F8D33A"></stop>
+        </linearGradient>
+        <linearGradient
+          id="paint5_linear_2708_18257"
+          x1="19.334"
+          x2="19.334"
+          y1="27.333"
+          y2="17"
+          gradientUnits="userSpaceOnUse">
+          <stop stopColor="#F0B90B"></stop>
+          <stop offset="1" stopColor="#F8D33A"></stop>
+        </linearGradient>
+      </defs>
+    </svg>
+  )
+}
+
+export default OneStopLearning
