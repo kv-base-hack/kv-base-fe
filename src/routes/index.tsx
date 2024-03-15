2cbed5aaeb12e41e2adac767964d80956375b4c0
Phong <phongpv@MacBook-Pro-cua-Phong.local>
2024-03-15 11:19:01 +0700

    fix: navigate route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,9 @@
-import { createFileRoute, useNavigate } from '@tanstack/react-router'
+import { Navigate, createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/')({
   component: Index,
 })
 
 function Index() {
-  const navigate = useNavigate()
-
-  return navigate({ to: '/onchain-discovery/onchain-signals' })
+  return <Navigate to="/onchain-discovery/onchain-signals" />
 }
