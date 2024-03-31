828d307450b989865397555eb0eab9052492a1d4
Phong <phongpv@MacBook-Pro-cua-Phong.local>
2024-03-31 16:43:35 +0700

    feat: kaichat ui

---
+++ src/components/shared/icons/kaichat/TopSMBuyIcon.tsx
@@ -0,0 +1,19 @@
+function TopSMBuyIcon({ className }: { className?: string }) {
+  return (
+    <svg
+      className={className}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      fill="none"
+      viewBox="0 0 24 24">
+      <path
+        stroke="#83BF6E"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M18 10l-3.434 3.434c-.198.198-.297.297-.412.334a.499.499 0 01-.309 0c-.114-.037-.213-.136-.41-.334l-2.87-2.868c-.197-.198-.296-.297-.41-.334a.499.499 0 00-.31 0c-.114.037-.213.136-.41.334L6 14m16-2c0 5.523-4.477 10-10 10S2 17.523 2 12 6.477 2 12 2s10 4.477 10 10z"></path>
+    </svg>
+  )
+}
+
+export default TopSMBuyIcon
