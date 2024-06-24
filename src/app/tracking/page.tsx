e91a63eca973e7dcb865379a25de6a1216b3661e
Pham Van Truong <97113715+truognpv@users.noreply.github.com>
2024-06-25 01:18:02 +0700

    update: tracking (#84)

--- src/app/tracking/page.tsx
+++ src/app/tracking/page.tsx
@@ -2,23 +2,14 @@
 
 import { ActivitySpotlight } from '@/components/Leaderboard/ActivitySpotlight'
 import { TableSMActivity } from '@/components/Leaderboard/Table/TableSMActivity'
-import { ActivityTabHeader } from '@/components/Leaderboard/Tabs/ActivityTabHeader'
 import { CardCommon } from '@/components/common/Card/CardCommon'
-import { ActiveTab } from '@/types/tabs/TabActivityHeader'
-import { useState } from 'react'
 
 export default function TrackingPae() {
-  const [activeTab, setActiveTab] = useState<ActiveTab>('Smart Money Activity')
-  const handleActiveTab = (value: ActiveTab) => {
-    setActiveTab(value)
-  }
-
   return (
     <div className="flex flex-col gap-4">
       <ActivitySpotlight limit={4} />
       <CardCommon>
-        <ActivityTabHeader {...{ activeTab, handleActiveTab }} />
-        <TableSMActivity {...{ activeTab }} />
+        <TableSMActivity />
       </CardCommon>
     </div>
   )
