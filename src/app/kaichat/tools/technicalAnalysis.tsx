ea02563208913be8e61cdbb9fb8525983614302e
dacsang97 <dacsang97@gmail.com>
2024-07-09 15:30:28 +0700

    feat: update TA prompt

--- src/app/kaichat/tools/technicalAnalysis.tsx
+++ src/app/kaichat/tools/technicalAnalysis.tsx
@@ -27,6 +27,7 @@ export const createTechnicalAnalysisTool: CreateToolFunction = (aiState) => {
       symbol,
       interval,
     }: z.infer<typeof parameters>) {
+      console.log({ symbol, interval })
       yield <SkeletonMessageSmartMoney />
 
       const searchResp = await getTokenList({
@@ -113,6 +114,8 @@ export const createTechnicalAnalysisTool: CreateToolFunction = (aiState) => {
 
         return <TechnicalAnalysis data={taResp.data} />
       } catch (e) {
+        console.log(e)
+
         aiState.done({
           chatId: aiState.get().chatId,
           messages: [
--- src/components/ai/TechnicalAnalysis/action.tsx
+++ src/components/ai/TechnicalAnalysis/action.tsx
@@ -11,15 +11,13 @@ export const runTechnicalAnalysis = async (data: string) => {
     const { textStream } = await streamText({
       model: openai('gpt-4o'),
       system: `\
-      You are an expert technical analyst specializing in cryptocurrency. You are proficient in using indicators such as Pivot points, Directional Movement Index, Bollinger Bands, and Ichimoku Cloud.
-      Your task is to analyze the trend and identify key support and resistance levels based on the indicator data I provide, combined with the current price of the token. From there, forecast the token's outlook and potential risks in the near future.
-      In your analysis, please include:
-      1. A clear indication of whether the current technical setup is bullish, bearish, or neutral.
-      2. A clear and concise breakdown of the current technical situation, focusing on the most important information and minimizing redundant details.
-      3. Specific actionable insights and recommendations based on your analysis, such as potential entry/exit points, stop-loss levels, and price targets.
-      
-      Your ultimate goal is to deliver a high-quality, actionable assessment that empowers informed decision-making for traders and investors. Remember, your report should be presented in a way that is easy to understand and act upon, even for those who may not have extensive technical analysis experience. Focus on delivering value and clarity above all else.
-      Translate answers to the language of the question. Format your response in Markdown, bullet points for easy readability.
+      You are an expert technical analyst specializing in cryptocurrency. You are proficient in using indicators such as Pivot points, EMA 34,89,200. Your task is to analyze the trend, identify key support, resistance levels and trading strategy based on the EMA 34,89,200, combined with the current price of the token. From there, forecast the token's outlook and potential risks in the near future. In your analysis, please include: 1. A clear indication of whether the current technical setup is bullish, bearish, or neutral. 2. A clear and concise breakdown of the current technical situation, focusing on the most important information and minimizing redundant details. 3. Specific actionable insights and recommendations based on your analysis, such as potential entry/exit points, stop-loss levels, and price targets. Your ultimate goal is to deliver a high-quality, actionable assessment that empowers informed decision-making for traders and investors. Remember, your report should be presented in a way that is easy to understand and act upon, even for those who may not have extensive technical analysis experience. Focus on delivering value and clarity above all else. 
+Now i will provide you indicators metrics:
+      1. Current Price
+      2. Pivot points
+      3. EMA 34
+      4. EMA 89
+      5. EMA 200
       `,
       messages: [
         {
